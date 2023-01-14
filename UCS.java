deb3f8198bf1ecee05d6d07cfaf7d69fb84790e5
Pearleen Tran <76570762+pearltran080@users.noreply.github.com>
2023-01-14 00:17:30 -0600

    UCS heap

--- UCS.java
+++ UCS.java
@@ -4,41 +4,36 @@ public class UCS {
     private Node goal;
     private Node initNode;
     private Node result;
-
-    private List<Node> leafNodes;
+    private PriorityQueue<Node> heap;
 
     public UCS(Node goal, Node initNode) {
         this.goal = goal;
         this.initNode = initNode;
         this.result = null;
-        this.leafNodes = new ArrayList<>();
+
+        this.heap = new PriorityQueue<Node>(new Comparator<Node>() {
+            @Override
+            public int compare(Node o1, Node o2) {
+
+                return Integer.compare(o1.book.getDepth(), o2.book.getDepth());
+            }
+        });
+
         run();
     }
 
     public void run() {
-        this.leafNodes.addAll(this.initNode.expand());
+        this.heap.addAll(this.initNode.expand());
         Node chosenOne = this.initNode;
 
-        while(leafNodes.size() != 0) {
+        while(heap.size() != 0) {
             if (chosenOne.equalsTo(this.goal)) {
                 this.result = chosenOne;
                 break;
             }
             else {
-                int lowest = Integer.MAX_VALUE;
-                int parentInd = -1;
-            
-                for (int i=0; i < leafNodes.size(); i++) {
-                    int currDepth = leafNodes.get(i).book.getDepth();
-                    if (currDepth < lowest ) {
-                        lowest = currDepth;
-                        chosenOne = leafNodes.get(i);
-                        parentInd = i;
-                    }
-                }
-                
-                this.leafNodes.addAll(chosenOne.expand());
-                this.leafNodes.remove(parentInd);
+                chosenOne = heap.poll();
+                heap.addAll(chosenOne.expand());
             }
         }
     }
